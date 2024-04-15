f810a892998c2bed15af89da9f637535c900caec
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-04-15 12:48:33 +0200

    added a custom method

--- vote/polls/models.py
+++ vote/polls/models.py
@@ -1,4 +1,7 @@
+import datetime 
+
 from django.db import models
+from django.utils import timezone
 
 #Here, each model is represented by a class that subclasses django.db.models.Model. Each model has a number of class variables, each of which represents a database field in the model.
 class Question(models.Model):
@@ -6,11 +9,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published") #creates column 'pub_date' storing date&time datatype
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def _str_(self):
-        def __str__(self):
-            return self.choice_text
+    def __str__(self):
+        return self.choice_text
