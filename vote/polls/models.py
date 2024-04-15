f0dd6c08384813dbf29595dabb237e478bd828e1
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-04-15 11:59:12 +0200

    completed class Choice

--- vote/polls/models.py
+++ vote/polls/models.py
@@ -5,3 +5,7 @@ class Question(models.model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
