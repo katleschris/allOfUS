d47eeaefd575e64a715d71f70931eac94b76c53f
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-04-15 12:41:39 +0200

    added '__str__()' method

--- vote/polls/models.py
+++ vote/polls/models.py
@@ -4,8 +4,13 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200) #creates column 'question_text' text(characters) datatype 
     pub_date = models.DateTimeField("date published") #creates column 'pub_date' storing date&time datatype
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def _str_(self):
+        def __str__(self):
+            return self.choice_text
