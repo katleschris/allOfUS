665149fdf6446e17e8708e96bd4b4eb64fcf81db
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-04-15 11:58:22 +0200

    Completed class Question

--- vote/polls/models.py
+++ vote/polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
