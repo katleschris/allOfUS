cbfb5806d2c8c0ecab2c6de28cd5d9aa8ff5cbe2
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-04-16 14:54:21 +0200

    Created view template

--- vote/polls/views.py
+++ vote/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 #In our poll application, we’ll have the following four views(type of webpage ):
@@ -9,8 +10,12 @@ from .models import Question
 #Question “index” page – displays the latest few questions.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = { "latest_question_list": latest_question_list}
+    #return HttpResponse(template.render(context, request))
+    #The render() function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument. 
+    #It returns an HttpResponse object of the given template rendered with the given context.
+    return render(request, "polls/index.html", context)
 
 #Question “detail” page – displays a question text, with no results but with a form to vote.
 def detail(request, question_id):
