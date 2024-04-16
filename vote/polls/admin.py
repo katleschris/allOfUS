bb892fcedd7c322f3260bdb084507d193331c518
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-04-16 12:15:13 +0200

    Made poll app modifiable in the admin

--- vote/polls/admin.py
+++ vote/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Question 
 
-# Register your models here.
+# Register your models here.we need to tell the admin that Question objects have an admin interface
+admin.site.register(Question)
