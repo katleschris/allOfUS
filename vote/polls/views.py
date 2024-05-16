aaa0b65cc6e29c2460dc1ac0da83ab220501238f
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-05-16 17:04:06 +0200

    Amended views

--- vote/polls/views.py
+++ vote/polls/views.py
@@ -1,33 +1,28 @@
+from django.db.models.query import QuerySet
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-#In our poll application, we’ll have the following four views(type of webpage ):
+class IndexView(generic.ListView):
+    template_name = "polls.index.html"
+    context_object_name = "latest_question_list"
 
-#Question “index” page – displays the latest few questions.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = { "latest_question_list": latest_question_list}
-    #return HttpResponse(template.render(context, request))
-    #The render() function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument. 
-    #It returns an HttpResponse object of the given template rendered with the given context.
-    return render(request, "polls/index.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-#Question “detail” page – displays a question text, with no results but with a form to vote.
-#The get_object_or_404() function takes a Django model as its first argument and an arbitrary number of keyword arguments, which it passes to the get() function of the model’s manager. It raises Http404 if the object doesn’t exist.
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-#Question “results” page – displays results for a particular question.
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 #Vote action – handles voting for a particular choice in a particular question.
 def vote(request, question_id):
